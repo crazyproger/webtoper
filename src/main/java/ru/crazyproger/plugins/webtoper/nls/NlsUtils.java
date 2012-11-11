7a51153ecc7427e34b9f388d8d90f6c561cff0ab
crazyproger <vladimir.rudev@gmail.com>
2012-11-11 10:42:57 +0400

    WT-1: refactoring - extract scope cooking into utils

---
+++ src/main/java/ru/crazyproger/plugins/webtoper/nls/NlsUtils.java
@@ -0,0 +1,38 @@
+package ru.crazyproger.plugins.webtoper.nls;
+
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.GlobalSearchScopes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.crazyproger.plugins.webtoper.config.ProjectConfig;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author crazyproger
+ */
+public class NlsUtils {
+    @Nullable
+    public static GlobalSearchScope getNlsScope(@NotNull Project project) {
+        ProjectConfig config = ServiceManager.getService(project, ProjectConfig.class);
+        List<String> folders = config.getFolders();
+        GlobalSearchScope scope = null;
+        for (String folder : folders) {
+            VirtualFile virtualFile = VfsUtil.findFileByIoFile(new File(folder), true);
+            if (virtualFile != null) {
+                GlobalSearchScope folderScope = GlobalSearchScopes.directoryScope(project, virtualFile, true);
+                if (scope == null) {
+                    scope = folderScope;
+                } else {
+                    scope.union(folderScope);
+                }
+            }
+        }
+        return scope;
+    }
+}
