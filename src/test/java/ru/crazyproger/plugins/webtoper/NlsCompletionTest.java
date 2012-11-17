01d66841fc41377c24cb41d0c9439b1e97659507
crazyproger <vladimir.rudev@gmail.com>
2012-11-17 17:31:30 +0400

    #2: make test stub

---
+++ src/test/java/ru/crazyproger/plugins/webtoper/NlsCompletionTest.java
@@ -0,0 +1,10 @@
+package ru.crazyproger.plugins.webtoper;
+
+import com.intellij.testFramework.LightCodeInsightTestCase;
+
+/**
+ * @author crazyproger
+ */
+public class NlsCompletionTest extends LightCodeInsightTestCase {
+    // todo implement
+}
