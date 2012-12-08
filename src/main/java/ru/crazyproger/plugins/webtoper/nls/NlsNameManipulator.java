a6a6ca07d375f521cf9557ebdffb53641902b999
crazyproger <vladimir.rudev@gmail.com>
2012-12-08 21:01:44 +0400

    #WT-5 fixed Implemented correct nls renaming

--- src/main/java/ru/crazyproger/plugins/webtoper/nls/NlsNameManipulator.java
+++ src/main/java/ru/crazyproger/plugins/webtoper/nls/NlsNameManipulator.java
@@ -1,8 +1,10 @@
 package ru.crazyproger.plugins.webtoper.nls;
 
+import com.google.common.base.Joiner;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.AbstractElementManipulator;
 import com.intellij.util.IncorrectOperationException;
+import org.apache.commons.lang.StringUtils;
 import ru.crazyproger.plugins.webtoper.nls.psi.impl.NlsNameImpl;
 
 /**
@@ -13,7 +15,12 @@ public class NlsNameManipulator extends AbstractElementManipulator<NlsNameImpl> 
     public NlsNameImpl handleContentChange(NlsNameImpl element, TextRange range, String newContent)
             throws IncorrectOperationException {
         final String oldText = element.getText();
-        String newText = oldText.substring(0, range.getStartOffset()) + newContent + oldText.substring(range.getEndOffset());
+        final String[] chunks = NlsUtils.nlsNameToPathChunks(oldText);
+        String newFileName = StringUtils.substringBeforeLast(newContent, ".");
+        assert chunks != null;
+        chunks[chunks.length - 1] = newFileName;
+        final String newNlsName = Joiner.on(".").join(chunks);
+        String newText = oldText.substring(0, range.getStartOffset()) + newNlsName + oldText.substring(range.getEndOffset());
         return (NlsNameImpl) element.replaceWithText(newText);
     }
 }
