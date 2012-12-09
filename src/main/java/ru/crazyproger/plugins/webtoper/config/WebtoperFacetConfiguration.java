d608e68e81da071b1849d8932f80b014106ef0a3
crazyproger <vladimir.rudev@gmail.com>
2012-12-10 01:19:00 +0400

    Facet configuration gui and saving/loading #WT-11

--- src/main/java/ru/crazyproger/plugins/webtoper/config/WebtoperFacetConfiguration.java
+++ src/main/java/ru/crazyproger/plugins/webtoper/config/WebtoperFacetConfiguration.java
@@ -20,67 +20,91 @@ import com.intellij.facet.FacetConfiguration;
 import com.intellij.facet.ui.FacetEditorContext;
 import com.intellij.facet.ui.FacetEditorTab;
 import com.intellij.facet.ui.FacetValidatorsManager;
+import com.intellij.openapi.components.PathMacroManager;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.InvalidDataException;
 import com.intellij.openapi.util.WriteExternalException;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jdom.Element;
-import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
+import java.io.File;
 
 /**
  * @author crazyproger
  */
 public class WebtoperFacetConfiguration implements FacetConfiguration {
+    public static final String WEBTOP_ROOT_LAYER = "Webtop";
+    public static final String PARENT_LAYER_ATTRIBUTE = "parentLayer";
+    public static final String NLS_ROOT_ATTRIBUTE = "nlsRoot";
     private WebtoperFacet facet;
+    private VirtualFile nlsRoot;
+    private String parentLayer;
+
+    @Nullable
+    public static String getModuleDirPath(Module module) {
+        String moduleFilePath = module.getModuleFilePath();
+        String moduleDirPath = new File(moduleFilePath).getParent();
+        if (moduleDirPath != null) {
+            moduleDirPath = FileUtil.toSystemIndependentName(moduleDirPath);
+        }
+        return moduleDirPath;
+    }
 
     @java.lang.Override
     public FacetEditorTab[] createEditorTabs(FacetEditorContext editorContext, FacetValidatorsManager validatorsManager) {
-        return new FacetEditorTab[]{new FacetEditorTab() {
-            @Nls
-            @java.lang.Override
-            public String getDisplayName() {
-                return "Webtoper facet";
-            }
-
-            @Nullable
-            @java.lang.Override
-            public JComponent createComponent() {
-                return new JLabel("Webtoper facet config");
-            }
-
-            @java.lang.Override
-            public boolean isModified() {
-                return false;  //To change body of implemented methods use File | Settings | File Templates.
-            }
-
-            @java.lang.Override
-            public void reset() {
-                //To change body of implemented methods use File | Settings | File Templates.
-            }
-
-            @java.lang.Override
-            public void disposeUIResources() {
-
-            }
-        }};
+        return new FacetEditorTab[]{new WebtoperFacetEditorTab(editorContext, this)};
     }
 
     @java.lang.Override
     public void readExternal(Element element) throws InvalidDataException {
-        //To change body of implemented methods use File | Settings | File Templates.
+        parentLayer = element.getAttributeValue(PARENT_LAYER_ATTRIBUTE);
+        String path = element.getAttributeValue(NLS_ROOT_ATTRIBUTE);
+        if (path != null) {
+            nlsRoot = LocalFileSystem.getInstance().findFileByPath(path);
+        } else {
+            nlsRoot = null;
+        }
     }
 
     @java.lang.Override
     public void writeExternal(Element element) throws WriteExternalException {
-        //To change body of implemented methods use File | Settings | File Templates.
+        element.setAttribute(PARENT_LAYER_ATTRIBUTE, parentLayer);
+        String path = nlsRoot == null ? null : toRelativePath(nlsRoot.getPath());
+        element.setAttribute(NLS_ROOT_ATTRIBUTE, path);
+
+    }
+
+    public WebtoperFacet getFacet() {
+        return facet;
     }
 
     public void setFacet(WebtoperFacet facet) {
         this.facet = facet;
     }
 
-    public WebtoperFacet getFacet() {
-        return facet;
+    public VirtualFile getNlsRoot() {
+        return nlsRoot;
+    }
+
+    public void setNlsRoot(VirtualFile nlsRoot) {
+        this.nlsRoot = nlsRoot;
+    }
+
+    public String getParentLayer() {
+        return parentLayer;
+    }
+
+    public void setParentLayer(String parentLayer) {
+        this.parentLayer = parentLayer;
+    }
+
+    @Nullable
+    private String toRelativePath(String absPath) {
+        absPath = FileUtil.toSystemIndependentName(absPath);
+        PathMacroManager macroManager = PathMacroManager.getInstance(getFacet().getModule());
+        return macroManager.collapsePath(absPath);
     }
 }
